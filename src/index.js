2223fa802a97003a7dd35827a0f14c6983742a54
jack-chapman <jackchapman093@gmail.com>
2019-01-23 20:47:49 +0000

    Add testing sandbox and click to remove censor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export default class Redacted {
+class Redacted {
   constructor(selector, options) {
     this.elements = document.querySelectorAll(selector);
     this.options = options;
@@ -8,7 +8,7 @@ export default class Redacted {
   createStyleNode() {
     const head = document.head || document.getElementsByTagName('head')[0];
 
-    const specialStyle = '.__redacted__ {position: relative;white-space: pre;}.__redacted__:after {background: black;border-radius: 0.1em;box-shadow: 0 0 1px rgba(0, 0, 0, 0.35);content: " ";width: 100%;height: 1.2em;left: 0;position: absolute;-webkit-transform: skewY(-5deg) rotate(5deg);transform: skewY(-5deg) rotate(5deg);}';
+    const specialStyle = '.__redacted__ {position: relative;white-space: pre;}.__redacted__:after {background: black;border-radius: 0.1em;box-shadow: 0 0 1px rgba(0, 0, 0, 0.35);content: " ";width: 100%;height: 1em;left: 0;top: 0;position: absolute;}';
 
     const styleEl = document.createElement('style');
     styleEl.type = 'text/css';
@@ -23,9 +23,13 @@ export default class Redacted {
   censor() {
     this.elements.forEach((element) => {
       element.classList.add('__redacted__');
+      element.setAttribute('title', 'REDACTED');
+      element.onclick = (e) => {
+        e.target.removeAttribute('title');
+        e.target.classList.remove('__redacted__');
+      };
     });
   }
 }
-// censors all h1 elements
-const redacted = new Redacted('h1');
-redacted.censor();
+
+window.Redacted = Redacted;
